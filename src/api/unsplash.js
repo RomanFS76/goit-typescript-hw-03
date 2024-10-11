6359f80b86b7cee010ac89f90a9384cc5dc2e2a5
RomanFS76 <korvy85@gmail.com>
2024-10-11 12:18:34 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com/"
-const KEY_API = "SPWQOYHFvR2Pvl2gJRcvBXyy6Wm-CFl80HRpfsb0okw";
+const KEY_API = "c5Fj6Lu1o7-m2UrVqvMUlR8r7rvHkIUekKVz-9edbYQ";
 
 export const getImagesApi = async (searchQuery,page) => {
     const {data} = await axios.get(`/search/photos/?client_id=${KEY_API}`, {params:{
